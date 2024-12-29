20fb12d75a94f3b69b35f43db0f7d4a0872b111f
kylebusey <kyledbuseybusiness@gmail.com>
2024-12-29 14:05:30 -0500

    added renderer script to display node version

--- my-electron-app/src/main.js
+++ my-electron-app/src/main.js
@@ -3,7 +3,10 @@ const { app, BrowserWindow } = require('electron')
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 800,
-    height: 600
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'src/preload.js')
+    }
   })
 
   win.loadFile('src/index.html')
